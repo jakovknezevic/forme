c59db38d1f3f044d46c7f6490c115d7d4269a13d
jakovknezevic <jakov.knezevic@skole.hr>
2016-03-08 18:26:06 +0100

    drugi commit

--- index.php
+++ index.php
@@ -7,5 +7,22 @@
 
 <body>
 <h1>Forme</h1>
+
+<p>
+
+<form>
+    Ime:<br>
+    <input type="text" name="ime"><br>
+    Prezime:<br>
+    <input type="text" name="prezime"><br>
+    <input type="submit" value="Pošalji!">
+</form>
+
+<?php
+echo 'Vaše ime je: ' . htmlspecialchars($_GET["ime"]) . '<br>';
+echo 'Vaše prezime je: ' . htmlspecialchars($_GET["prezime"]);
+?>
+
+</p>
 </body>
 </html>
