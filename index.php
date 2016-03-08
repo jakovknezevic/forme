8d7d27a87560c5b2233a94d27629ffb1338e940b
jakovknezevic <jakov.knezevic@skole.hr>
2016-03-08 17:33:37 +0100

    Procistili smo PHP.

--- index.php
+++ index.php
@@ -1,29 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Naslov</title>
     <meta charset="UTF-8">
+    <title>Forme</title>
 </head>
 
 <body>
-<h1>Prvi</h1>
-
-<p>
-    <?php
-    echo('Pozdrav iz PHP-a');
-    echo('<br>');
-    echo(date('d.m.Y.'));
-    ?>
-</p>
-<p>
-
-    <?php
-    $date1 = date('Y-m-d');
-    $date2 = date('2016-03-18');
-    $diff = date_diff($date1,$date2);
-    echo('Do praznika je ostalo još: <b>' . $diff . '</b>');
-    ?>
-
-</p>
+<h1>Forme</h1>
 </body>
 </html>
