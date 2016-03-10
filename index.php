9143f8ab3890da4bd1c716185d26c996872e26e4
Jakov Knežević <jakov.knezevic@skole.hr>
2016-03-10 13:55:10 +0100

    Dodali smo css i neke izmjene u php-u.

--- index.php
+++ index.php
@@ -10,17 +10,75 @@
 
 <p>
 
-<form>
+<form method="post">
     Ime:<br>
     <input type="text" name="ime"><br>
     Prezime:<br>
     <input type="text" name="prezime"><br>
-    <input type="submit" value="Pošalji!">
+    Spol:<br>
+    <input type="radio" name="spol" value="M">Muško<br>
+    <input type="radio" name="spol" value="Ž">Žensko<br>
+    <input type="radio" name="spol" value="JD">Nešto treće<br>
+    <input type="submit" value="Submit">
 </form>
 
 <?php
-echo 'Vaše ime je: ' . htmlspecialchars($_GET["ime"]) . '<br>';
-echo 'Vaše prezime je: ' . htmlspecialchars($_GET["prezime"]);
+if ($_POST['spol'] == 'M')
+{
+    echo 'Vaše ime je: ' . "<p class='musko'>" . htmlspecialchars($_POST["ime"]) . '<br>';
+    echo 'Vaše prezime je: ' . htmlspecialchars($_POST["prezime"]) . '<br>';
+    echo 'Spol: ' . htmlspecialchars($_POST["spol"]) . "</p class='musko'><br>";
+}
+
+elseif ($_POST['spol'] == 'Ž')
+{
+    echo 'Vaše ime je: ' . "<p class='zensko'>" . htmlspecialchars($_POST["ime"]) . '<br>';
+    echo 'Vaše prezime je: ' . htmlspecialchars($_POST["prezime"]) . '<br>';
+    echo 'Spol: ' . htmlspecialchars($_POST["spol"]) . "</p class='zensko'><br>";
+}
+
+elseif ($_POST['spol'] == 'JD')
+{
+    echo 'Vaše ime je: ' . "<p class='jd'>" . htmlspecialchars($_POST["ime"]) . '<br>';
+    echo 'Vaše prezime je: ' . htmlspecialchars($_POST["prezime"]) . '<br>';
+    echo 'Spol: ' . htmlspecialchars($_POST["spol"]) . "</p class='jd'><br>";
+}
+
+
+//echo 'Vaše ime je: ' . "<p class='musko" . htmlspecialchars($_POST["ime"]) . '<br>';
+//echo 'Vaše prezime je: ' . htmlspecialchars($_POST["prezime"]) . '<br>';
+//echo 'Spol: ' . htmlspecialchars($_POST["spol"]) . '<br>';
+
+if ($_POST['spol'] == 'M')
+{
+    echo ('Vi ste pravi muškarac!');
+}
+elseif ($_POST['spol'] == 'Ž')
+{
+    echo ('Vi ste prava žena!');
+}
+elseif  ($_POST['spol'] == 'JD')
+{
+    echo ('Što ste vi!?');
+}
+
+
+if ($_POST['ime'] and $_POST['prezime'] == '')
+{
+    echo ('Niste unesli ime i prezime!');
+}
+elseif ($_POST['ime'] == '')
+{
+    echo ('Niste unesli ime!');
+}
+elseif ($_POST['prezime'] == '')
+{
+    echo('Niste unesli prezime!');
+}
+else
+{
+    echo('');
+}
 ?>
 
 </p>
